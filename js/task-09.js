03dec33ecb76e20963c0b2206b81d2281a29545f
Valeria-Kichanova <kvv.dnipro@gmail.com>
2022-11-09 19:00:02 +0300

    09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const ref = {
+  btn: document.querySelector('button.change-color'),
+  body: document.body,
+  colorSpan: document.querySelector('span.color'),
+};
+ref.btn.addEventListener('click', onClickBtn);
+
+function onClickBtn() {
+  const color = getRandomHexColor();
+  ref.body.style.backgroundColor = color;
+  ref.colorSpan.textContent = color;
+}
